8b9cd568e5d55cd1309c1c8fbb766425a75ad26d
Luming Yin <vista980622@gmail.com>
2019-04-28 15:11:57 -0400

    Additional (unsuccessful) attempts to fix crashes on 10.11 through swizzling.

--- Caption/Others/NSSet+Additions.m
+++ Caption/Others/NSSet+Additions.m
@@ -11,46 +11,52 @@
 
 @implementation NSSet (Additions)
 + (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
+    if (@available(macOS 10.12, *)) {
+    } else {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            Class class = [self class];
 
-        SEL originalSelector = @selector(intersectsSet:);
-        SEL swizzledSelector = @selector(swz_intersectsSet:);
+            SEL originalSelector = @selector(intersectSet:);
+            SEL swizzledSelector = @selector(swz_intersectsSet:);
 
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+            Method originalMethod = class_getInstanceMethod(class, originalSelector);
+            Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
 
-        // When swizzling a class method, use the following:
-        // Class class = object_getClass((id)self);
-        // ...
-        // Method originalMethod = class_getClassMethod(class, originalSelector);
-        // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
+            // When swizzling a class method, use the following:
+            // Class class = object_getClass((id)self);
+            // ...
+            // Method originalMethod = class_getClassMethod(class, originalSelector);
+            // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
 
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod));
+            BOOL didAddMethod =
+            class_addMethod(class,
+                            originalSelector,
+                            method_getImplementation(swizzledMethod),
+                            method_getTypeEncoding(swizzledMethod));
 
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
+            if (didAddMethod) {
+                class_replaceMethod(class,
+                                    swizzledSelector,
+                                    method_getImplementation(originalMethod),
+                                    method_getTypeEncoding(originalMethod));
+            } else {
+                method_exchangeImplementations(originalMethod, swizzledMethod);
+            }
+        });
+    }
 }
 
 - (void)swz_intersectsSet:(id)arg1 {
     NSString *argType = NSStringFromClass([arg1 class]);
     NSSet *set;
     BOOL shouldPrint = YES;
-    if ([arg1 isKindOfClass:[NSMutableSet class]]) {
+    if ([arg1 isKindOfClass:[NSMutableOrderedSet class]]) {
+        set = ((NSMutableOrderedSet *)arg1).set;
+    } else if ([arg1 isKindOfClass:[NSOrderedSet class]]) {
         set = ((NSOrderedSet *)arg1).set;
-        shouldPrint = NO;
+    } else if ([arg1 isKindOfClass:[NSMutableSet class]]) {
+        set = [((NSMutableSet *)arg1) copy];
     } else if ([arg1 isKindOfClass:[NSSet class]]) {
         set = arg1;
     } else {
